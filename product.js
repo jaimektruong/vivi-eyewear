e21fa7bce1f075bb291ddbe6cba4b39a7eda1d55
vananh2003 <anhttv21406@st.uel.edu.vn>
2023-12-27 22:10:35 +0700

    bổ sung file

---
+++ product.js
@@ -0,0 +1,102 @@
+[
+    {
+        "name": "GKL000001",
+        "price": 500000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000002",
+        "price": 750000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Xam",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000003",
+        "price": 400000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000004",
+        "price": 700000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000005",
+        "price": 450000,
+        "type": "gong-kinh",
+        "material":"Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000006",
+        "price": 600000,
+        "type": "gong-kinh",
+        "material":"Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Nau",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000007",
+        "price": 800000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000008",
+        "price": 760000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000009",
+        "price": 850000,
+        "type": "gong-kinh",
+        "material":"Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+    {
+        "name": "GKL000010",
+        "price": 700000,
+        "type": "gong-kinh",
+        "material": "Kim loai",
+        "shape": "Aviator",
+        "size": "53 - 19 - 140",
+        "color": "Đen",
+        "description": "Gọng kính này thuộc bộ sưu tập Axeden, được chế tạo từ một loại kim loại đặc biệt - một vật liệu nhẹ và cực kỳ bền bỉ. Quá trình sản xuất thủ công diễn ra tại các cơ sở gia công ở Canada, nơi nổi tiếng với chất lượng sản xuất cao cùng với sự chuyên nghiệp"
+    },
+]
