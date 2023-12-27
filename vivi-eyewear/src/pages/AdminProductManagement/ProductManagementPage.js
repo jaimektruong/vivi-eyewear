fae30c16d209570bfe70171a691ba8c243c7b36e
Ainhi29 <95375193+Ainhi29@users.noreply.github.com>
2023-12-27 22:21:32 +0700

    json change

---
+++ vivi-eyewear/src/pages/AdminProductManagement/ProductManagementPage.js
@@ -0,0 +1,10 @@
+import "./ProductManagementPage.scss";
+function ProductManagementPage() {
+  return (
+    <div className="inner">
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default ProductManagementPage;
